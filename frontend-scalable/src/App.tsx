96c187bd25b25cb1e3536f3fee98330d7ab749d1
Chuch3x <c.augusto_mendez@hotmail.com>
2025-08-30 16:38:09 -0400

    vite added

---
+++ frontend-scalable/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>SCALABLE JUAN </h1>
+        <h2>SCALABLE JUAN </h2>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
