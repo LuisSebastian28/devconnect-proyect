3bb0629e23849e759fae6cb14c4da14e3e4d62ff
Chuch3x <c.augusto_mendez@hotmail.com>
2025-08-30 20:22:31 -0400

    corrected navbar styles

--- frontend-scalable/src/components/Navbar.tsx
+++ frontend-scalable/src/components/Navbar.tsx
@@ -26,31 +26,31 @@ export const Navbar = () => {
         <div className="container mx-auto px-4 py-4">
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-4 cursor-crosshair">
-              <div className="flex items-center space-x-2">
-                <div className="w-8 h-8 bg-accent rounded-lg flex items-center justify-center bg-purple-500">
-                  <DollarSign className="w-5 h-5 text-accent-foreground" />
+              <div className="flex items-center space-x-2 mr-10">
+                <div className="w-8 h-8 bg-accent rounded-lg flex items-center justify-center bg-indigo-600">
+                  <DollarSign className="w-5 h-5 text-white" />
                 </div>
                 <h1 className="text-xl font-bold text-foreground">CrowdLend</h1>
               </div>
-                <div className="flex flex-1 justify-end">
+              <div className="flex flex-1 justify-end">
                 <nav className="hidden md:flex items-center">
                   <div className="flex gap-8">
-                  {navItems.map((item) => (
-                    <Button
-                    key={item.key}
-                    variant={activeTab === item.key ? "default" : "ghost"}
-                    onClick={() => {
-                      setActiveTab(item.key);
-                      //navigate(item.path);
-                    }}
-                    className=" border full-rounded transition-all hover:font-bold hover:text-lg hover:bg-purple-100 cursor-pointer"
-                    >
-                    {item.label}
-                    </Button>
-                  ))}
+                    {navItems.map((item) => (
+                      <Button
+                        key={item.key}
+                        variant={activeTab === item.key ? "default" : "ghost"}
+                        onClick={() => {
+                          setActiveTab(item.key);
+                          //navigate(item.path);
+                        }}
+                        className=" border full-rounded transition-all hover:font-bold hover:text-lg hover:bg-purple-100 cursor-pointer"
+                      >
+                        {item.label}
+                      </Button>
+                    ))}
                   </div>
                 </nav>
-                </div>
+              </div>
             </div>
             <div className="flex items-center space-x-4">{ConnectWallet()}</div>
           </div>
